27899dab60a4115957be376768683a50f9c992aa
fzx <fzx@166.com>
2020-10-19 17:00:42 +0800

    Login模块新增接口

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -7,16 +7,29 @@
  */
 
 namespace app\admin\controller;
+
 use think\Controller;
+use think\Db;
+use think\Request;
 
 
 class Login extends Controller
 {
-    public function signin(){
-        return $this->fetch();
+    // 登录
+    public function signin(Request $request){
+        $data = $request->param('data');
+        $user = User::where('user_name', $data)->find();
+//        $user = Db::table('lab_user')->select();
+        var_dump($user);
     }
 
+    // 忘记密码
     public function forget(){
-        return $this->fetch();
+
+    }
+
+    // 登出
+    public function logout(){
+
     }
 }
